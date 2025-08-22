a7b2cb7b28c212200899a7c21795018cb48fee17
Dipanshu Sahoo <148225619+Dipanshu789@users.noreply.github.com>
2025-08-22 22:14:50 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,11 @@
+
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-
+import { liveLocations } from '../shared/schema';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema: { liveLocations } });
